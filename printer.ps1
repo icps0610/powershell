0024b32ce893d87c39e75f3275e7b1ee8bb2e5b7
icps0610 <icps0610@gmail.com>
2025-07-15 17:00:23 +0800

    2025-07-15-17:00:23

--- printer.ps1
+++ printer.ps1
@@ -1,7 +1,9 @@
 $ip = "192.168.0.201"
 $printName = "印表機名稱"
 
-# 使用的 port
-Add-PrinterPort -Name $ip -PrinterHostAddress $ip
+# 如果 Port 不存在才新增
+if (-not (Get-PrinterPort -Name $ip -ErrorAction SilentlyContinue)) {
+    Add-PrinterPort -Name $ip -PrinterHostAddress $ip
+}
 # 新增
 Add-Printer -Name "$printName" -PortName "$ip" -DriverName "Microsoft Print to PDF"
